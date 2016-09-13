5cdcb6eb5091b40a4ede878f9d64d06f1ad8dd45
Tim Lamberton <t.lamberton@uq.edu.au>
2016-09-13 12:36:51 +1000

    fix error

--- groopm/cluster.py
+++ groopm/cluster.py
@@ -194,7 +194,7 @@ class ProfileDistanceEngine:
     
     @profile
     def makeNormRanks(self, covProfiles, kmerSigs, contigLengths, silent=False):
-        (cov_ranks, kmer_ranks, weights) = self.makeScaledRanks(self, covProfiles, kmerSigs, contigLengths, silent=silent)
+        (cov_ranks, kmer_ranks, weights) = self.makeScaledRanks(covProfiles, kmerSigs, contigLengths, silent=silent)
         rank_norms = np.sqrt(cov_ranks**2 + kmer_ranks**2)
         return (rank_norms, weights)
     
