1e5fd308632f367fa0f1e4d8674dbc40b23d056d
Tim Lamberton <t.lamberton@uq.edu.au>
2016-11-15 11:51:59 +1000

    merge latest stream extension file and test from main branch

--- groopm/stream_ext.pyx
+++ groopm/stream_ext.pyx
@@ -1,4 +1,4 @@
-# stream.pyx
+# stream_ext.pyx
 
 import cython
 #cimport numpy as np
@@ -17,7 +17,6 @@ def merge(x_len,
     i = 0
     j = 0
     for k in range(out_len):
-        print x[i] if i < x_len else -1, y[j] if j < y_len else -1
         if j < y_len  and (i==x_len or y[j] < x[i]):
             out[k] = y[j]
             out_inds[k] = y_inds[j]
@@ -27,9 +26,8 @@ def merge(x_len,
             out[k] = x[i]
             out_inds[k] = x_inds[i]
             i += 1
-        print out[k]
-
     return (i, j)
+        
     
     
 ###############################################################################
